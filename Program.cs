f502db64d3153d7bc6aee34b5ce1718cc1db098f
miguel <miguelpositivo02@gmail.com>
2023-04-18 20:02:43 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("digite uma frase:");
+string palavra = Console.ReadLine()!;
+string resoluçao = palavra.Replace (@"chato","#@$%*!&");
+string soluçao =$" Eu sou uma criança educada. No seu lugar eu diria :{resoluçao}";
+Console.WriteLine(soluçao);
